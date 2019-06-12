2fc02eae114b07132b17caca3a53c0811445b3a7
abourass <abourassa@assetval.com>
2019-06-11 22:09:42 -0600

    Created more utils / models

---
+++ dist/dataStructures/models/node.js
@@ -0,0 +1,14 @@
+'use strict';
+
+class Node {
+  constructor(key) {
+    this.key = key;
+    this.left = undefined;
+    this.right = undefined;
+  }
+
+  toString() {
+    return `${this.key}`;
+  }
+}
+module.exports.Node = Node;
---
+++ dist/dataStructures/models/valuePairLazy.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const {ValuePair} = require('./valuePair');
+
+class ValuePairLazy extends ValuePair {
+  constructor(key, value, isDeleted = false) {
+    super(key, value);
+    this.key = key;
+    this.value = value;
+    this.isDeleted = isDeleted;
+  }
+}
+module.exports.ValuePairLazy = ValuePairLazy;
