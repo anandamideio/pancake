49e8eaf7af272f0681fc6cc43dd29e833d39bead
abourass <abourassa@assetval.com>
2019-06-12 22:57:31 -0600

    Finished all data structures

---
+++ dist/dataStructures/models/redBlackNode.js
@@ -0,0 +1,17 @@
+"use strict";
+
+const {Node} = require("./node");
+
+class RedBlackNode extends Node {
+  constructor(key) {
+    super(key);
+    this.key = key;
+    this.color = Colors.RED; // {6}
+    this.parent = null; // {7}
+  }
+
+  isRed() {
+    return this.color === Colors.RED;
+  }
+}
+exports.RedBlackNode = RedBlackNode;
